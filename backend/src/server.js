32891d684becb9822d63071cceb2b6cb55ebc9d7
ambikaaswal <aswalambika@gmail.com>
2026-09-17 16:42:49 +0530

    depolyment changes

--- backend/src/server.js
+++ backend/src/server.js
@@ -32,6 +32,6 @@ app.use((err, req, res, next) => {
   res.status(err.status || 500).json({ error: err.message || 'Internal server error' });
 });
 
-app.listen(PORT, () => {
-  console.log(`Backend running on http://localhost:${PORT}`);
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Backend running on ${PORT}`);
 });
